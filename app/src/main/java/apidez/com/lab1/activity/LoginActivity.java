2356419209e4ba999d7f6ddad9c362f605e2eeb3
Daniel Weibel <danielmweibel@gmail.com>
2017-02-16 20:39:49 +0700

    Assignment complete

--- app/src/main/java/apidez/com/lab1/activity/LoginActivity.java
+++ app/src/main/java/apidez/com/lab1/activity/LoginActivity.java
@@ -1,7 +1,10 @@
 package apidez.com.lab1.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import apidez.com.lab1.R;
@@ -12,7 +15,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        // TODO: Insert your code
+
+        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, StreamActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 
     private void showToast(String message) {
--- app/src/main/java/apidez/com/lab1/adapter/PostAdapter.java
+++ app/src/main/java/apidez/com/lab1/adapter/PostAdapter.java
@@ -2,14 +2,18 @@ package apidez.com.lab1.adapter;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.List;
 
+import apidez.com.lab1.R;
 import apidez.com.lab1.model.Post;
 
 /**
@@ -32,18 +36,35 @@ public class PostAdapter extends ArrayAdapter<Post> {
     @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        // TODO: Insert your code
-        // bindViewHolder(position, viewHolder)
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_post, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.tvUsername = (TextView) convertView.findViewById(R.id.tvUsername);
+            viewHolder.tvDescription = (TextView) convertView.findViewById(R.id.tvDescription);
+            viewHolder.tvDate = (TextView) convertView.findViewById(R.id.tvDate);
+            viewHolder.ivAvatar = (ImageView) convertView.findViewById(R.id.ivAvatar);
+            viewHolder.ivImage = (ImageView) convertView.findViewById(R.id.ivImage);
+            bindViewHolder(position, viewHolder);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        bindViewHolder(position, viewHolder);
         return convertView;
     }
 
     private void bindViewHolder(int position, ViewHolder viewHolder) {
         Post post = mPosts.get(position);
-        // TODO: Insert your code
+        viewHolder.tvUsername.setText(post.getUsername());
+        viewHolder.tvDescription.setText(post.getDescription());
+        viewHolder.tvDate.setText(post.getDate());
+        loadImage(viewHolder.ivImage, post.getImage());
+        loadImage(viewHolder.ivAvatar, post.getAvatar());
     }
 
     private void loadImage(ImageView imageView, String path) {
-        // TODO: Insert your code
+        Picasso.with(getContext()).load(path).placeholder(R.drawable.placeholder).into(imageView);
     }
 
     public class ViewHolder {
